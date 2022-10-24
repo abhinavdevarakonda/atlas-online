cb50b5ea3e4e950f576c1777e523f82cedb85d8a
abhinavdevarakonda <50144533+abhinavdevarakonda@users.noreply.github.com>
2022-10-24 09:26:20 +0530

    country rule restriciton
    now players can only input countries that are actually present in the country list

--- atlas/client.py
+++ atlas/client.py
@@ -11,6 +11,21 @@ MSG_REQ = "your turn: "
 
 user_name = input("Enter your name: ")
 
+with open("country_list.txt","r") as list:
+    countries = list.readlines()
+
+    for i in range(len(countries)):
+        countries[i] = (countries[i].strip()).lower()
+
+
+def wordCheck(player_country):
+    player_country = player_country.lower()
+    if player_country in countries:
+        return True
+    else:
+        print(player_country)
+        return False
+
 def receive():
     while True:
         try:
@@ -18,8 +33,18 @@ def receive():
             if server_msg == NAME_REQ:
                 client.send(user_name.encode('ascii'))
             elif server_msg == MSG_REQ:
-                message = f"[{user_name}]: {input(MSG_REQ)}"
-                client.send(message.encode('ascii'))
+
+                player_country = input(MSG_REQ)
+                if wordCheck(player_country):
+                    message = f"[{user_name}]: {player_country}"
+                    client.send(message.encode('ascii'))
+                else:
+                    while wordCheck(player_country)==False:
+                        print("invalid. country not in country list.")
+                        player_country = input(MSG_REQ)
+
+                    message = f"[{user_name}]: {player_country}"
+                    client.send(message.encode('ascii'))
             else:
                 print(server_msg)
 
